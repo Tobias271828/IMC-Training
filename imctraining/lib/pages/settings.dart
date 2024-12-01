0497049cde07ef01aab3586018ab8077b048bfc5
Tobias <>
2024-12-01 01:19:59 +0100

    Kleine Änderungen (Alle Sprachen jetzt im Bundle)

--- imctraining/lib/pages/settings.dart
+++ imctraining/lib/pages/settings.dart
@@ -66,42 +66,42 @@ class _SettingsState extends State<Settings> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 1 (${(100*context.watch<CurrentExerciseProvider>().aufgaben1geloest / context.watch<CurrentExerciseProvider>().aufgaben1gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 1: ${(100*context.watch<CurrentExerciseProvider>().aufgaben1geloest / context.watch<CurrentExerciseProvider>().aufgaben1gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben1, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(1);},)
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 2 (${(100*context.watch<CurrentExerciseProvider>().aufgaben2geloest / context.watch<CurrentExerciseProvider>().aufgaben2gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 2: ${(100*context.watch<CurrentExerciseProvider>().aufgaben2geloest / context.watch<CurrentExerciseProvider>().aufgaben2gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben2, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(2);},)
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 3 (${(100*context.watch<CurrentExerciseProvider>().aufgaben3geloest / context.watch<CurrentExerciseProvider>().aufgaben3gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 3: ${(100*context.watch<CurrentExerciseProvider>().aufgaben3geloest / context.watch<CurrentExerciseProvider>().aufgaben3gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben3, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(3);},)
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 4 (${(100*context.watch<CurrentExerciseProvider>().aufgaben4geloest / context.watch<CurrentExerciseProvider>().aufgaben4gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 4: ${(100*context.watch<CurrentExerciseProvider>().aufgaben4geloest / context.watch<CurrentExerciseProvider>().aufgaben4gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben4, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(4);},)
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 5 (${(100*context.watch<CurrentExerciseProvider>().aufgaben5geloest / context.watch<CurrentExerciseProvider>().aufgaben5gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 5: ${(100*context.watch<CurrentExerciseProvider>().aufgaben5geloest / context.watch<CurrentExerciseProvider>().aufgaben5gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben5, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(5);},)
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("${AppLocalizations.of(context)!.showproblems} 6 (${(100*context.watch<CurrentExerciseProvider>().aufgaben6geloest / context.watch<CurrentExerciseProvider>().aufgaben6gesamt).round()}% ${AppLocalizations.of(context)!.solved})"),
+              Text("${AppLocalizations.of(context)!.showproblems} 6: ${(100*context.watch<CurrentExerciseProvider>().aufgaben6geloest / context.watch<CurrentExerciseProvider>().aufgaben6gesamt).round()}% ${AppLocalizations.of(context)!.solved}"),
               Checkbox(value: context.watch<CurrentExerciseProvider>().zeigeaufgaben6, onChanged: (value){context.read<CurrentExerciseProvider>().aenderezeigeaufgabenbestimmertnummer(6);},)
             ],
           ),
